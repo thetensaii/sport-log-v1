75629ab1c10376f00a4769ccae17be21520b2716
TheTensaii <f.salimou@hotmail.fr>
2024-01-16 19:00:01 -0500

    feat: change player password

--- apps/fronts/front-office/src/player/Home.tsx
+++ apps/fronts/front-office/src/player/Home.tsx
@@ -1,19 +1,9 @@
+import { useState } from "react"
 import { HStack } from "@sport-log/ui"
-import { Modal } from "@sport-log/ui"
-import { ModalContent } from "@sport-log/ui"
-import { ModalCloseButton } from "@sport-log/ui"
-import { ModalFooter } from "@sport-log/ui"
-import { FormLabel } from "@sport-log/ui"
-import { Input } from "@sport-log/ui"
-import { Select } from "@sport-log/ui"
-import { Checkbox } from "@sport-log/ui"
-import { FormControl } from "@sport-log/ui"
-import { ModalBody } from "@sport-log/ui"
-import { ModalHeader } from "@sport-log/ui"
-import { ModalOverlay } from "@sport-log/ui"
 import { Button } from "@sport-log/ui"
 import { Center, Heading, Text, VStack, Box, Link } from "@sport-log/ui"
-import { useState } from "react"
+import { EditProfileModal } from "./EditProfileModal"
+import { ChangePasswordModal } from "./ChangePasswordModal"
 
 export const Home = () => {
   const player = {
@@ -50,6 +40,14 @@ export const Home = () => {
   }
   const closeEditProfileModal = () => {
     setIsEditProfileModalOpen(false)
+  }
+
+  const [isChangePasswordModalopen, setIsChangePasswordModalOpen] = useState<boolean>(false);
+  const openChangePasswordModal = () => {
+    setIsChangePasswordModalOpen(true)
+  }
+  const closeChangePasswordModal = () => {
+    setIsChangePasswordModalOpen(false)
   }
 
   return ( 
@@ -101,150 +99,12 @@ export const Home = () => {
 
           <HStack spacing="8">
             <Button colorScheme="blue" size="lg" onClick={openEditProfileModal}>Éditer profil</Button>
-            <Button colorScheme="red" size="lg">Changer mot de passe</Button>
+            <Button colorScheme="red" size="lg" onClick={openChangePasswordModal}>Changer mot de passe</Button>
           </HStack>
         </VStack>
       </Center>
-      <Modal isOpen={isEditProfileModalOpen} onClose={closeEditProfileModal} size="xl">
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>
-            <Text fontSize="3xl">Éditer votre profil</Text>
-          </ModalHeader>
-          <ModalCloseButton />
-
-          <ModalBody>
-            <VStack spacing="4" alignItems='flex-start'>
-            <Text fontSize="2xl" fontWeight='bold'>Infos Générales</Text>
-            <HStack w="full" spacing="6">
-              <FormControl>
-                <FormLabel>Nom</FormLabel>
-                <Input type="text" />
-              </FormControl>
-
-              <FormControl>
-                <FormLabel>Prénom</FormLabel>
-                <Input type="text" />
-              </FormControl>
-            </HStack>
-
-            <FormControl>
-              <FormLabel>Sexe</FormLabel>
-              <Select placeholder="Choisissez un sexe">
-                <option value="homme">Homme</option>
-                <option value="femme">Femme</option>
-              </Select>
-            </FormControl>
-
-            <FormControl>
-              <FormLabel>Date de naissance</FormLabel>
-              <Input type="date" />
-            </FormControl>
-
-            <FormControl>
-              <FormLabel>Numéro de portable</FormLabel>
-              <Input type="tel" />
-            </FormControl>
-
-            <FormControl>
-              <FormLabel>Email</FormLabel>
-              <Input type="email" />
-            </FormControl>
-
-              <Text fontSize="2xl" fontWeight='bold'>Infos sportives</Text>
-              <FormControl>
-                <FormLabel>Poste(s)</FormLabel>
-                <HStack spacing="8">
-                  <Checkbox name="position" value="1">Meneur</Checkbox>
-                  <Checkbox name="position" value="2">Arrière</Checkbox>
-                  <Checkbox name="position" value="3">Ailier</Checkbox>
-                  <Checkbox name="position" value="4">Ailier Fort</Checkbox>
-                  <Checkbox name="position" value="5">Pivot</Checkbox>
-                </HStack>
-              </FormControl>
-
-              <FormControl>
-                <FormLabel>Taille (cm)</FormLabel>
-                <Input type="number" />
-              </FormControl>
-
-              <FormControl>
-                <FormLabel>Club actuel</FormLabel>
-                <Input />
-              </FormControl>
-
-              <HStack spacing="8" width="full">
-                <FormControl>
-                  <FormLabel>Division (ProB, N1, R3, etc)</FormLabel>
-                  <Input />
-                </FormControl>
-
-                <FormControl>
-                  <FormLabel>Coach</FormLabel>
-                  <Input />
-                </FormControl>
-              </HStack>
-
-
-              
-              <HStack spacing="8" width="full">
-                <FormControl>
-                  <FormLabel>Lien vers CV Professionnel</FormLabel>
-                  <Input />
-                </FormControl>
-
-                <FormControl>
-                  <FormLabel>Lien vers CV Sportif</FormLabel>
-                  <Input />
-                </FormControl>
-              </HStack>
-
-              
-              <HStack spacing="8" width="full">
-                <FormControl>
-                  <FormLabel>Lien vers profil Proballers</FormLabel>
-                  <Input />
-                </FormControl>
-
-                <FormControl>
-                  <FormLabel>Lien vers vidéo de présentation</FormLabel>
-                  <Input />
-                </FormControl>
-              </HStack>
-
-              <FormControl>
-                <FormLabel>Lien vers vidéo d'highlights</FormLabel>
-                <Input />
-              </FormControl>
-
-              <Text fontSize="2xl" fontWeight='bold'>Réseaux Sociaux</Text>
-              <HStack spacing="8" width="full">
-                <FormControl>
-                  <FormLabel>Lien Instagram</FormLabel>
-                  <Input />
-                </FormControl>
-                <FormControl>
-                  <FormLabel>Lien Facebook</FormLabel>
-                  <Input />
-                </FormControl>
-                <FormControl>
-                  <FormLabel>Lien Twitter</FormLabel>
-                  <Input />
-                </FormControl>
-              </HStack>
-            </VStack>
-
-          </ModalBody>
-
-          <ModalFooter>
-            <HStack spacing="2" width="full" justifyContent="end">
-              <Button variant="outline" colorScheme="blue" size="lg" onClick={closeEditProfileModal}>Retour</Button>
-              <Button colorScheme="blue" size="lg">Enregistrer</Button>
-            </HStack>
-          </ModalFooter>
-        </ModalContent>
-         
-      </Modal>
+      <EditProfileModal isOpen={isEditProfileModalOpen} closeModal={closeEditProfileModal} />
+      <ChangePasswordModal isOpen={isChangePasswordModalopen} closeModal={closeChangePasswordModal} />
     </>
   )
 }
